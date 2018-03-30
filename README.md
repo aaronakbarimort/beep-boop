a6b5b524015b2f8bd334c49f6c9e73859eb915f9
Aaron Akbari-Mort <aaronakbarimort@gmail.com>
2018-03-30 14:43:10 -0700

    add starter JavaScript & more detailed Specs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Portfolio
 
-#### Track Suggester for Epicodus, 03.23.2018
+#### Beep Boop for Epicodus, 03.30.2018
 
 ## By Aaron Akbari-Mort
 
@@ -10,19 +10,11 @@
 
 Website featuring a course Beep Boop form for Epicodus week 3 Independent Project.
 
-### Specs
+## Specs
 
-| Spec | Input | Output |
-| **Program replaces 0 with "Beep!" | User Input: 0 | Output: ["Beep!"] |
-
-
-| :-------------     | :------------- | :------------- |
-| **Homepage** | User accesses localhost:5004 | Homepage with user input form |
-| **Program Gathers User Input** | User input: "pants" | Output: "pants" |
-| **Program Removes Spaces from User Input**| User Input: "pants pants" | Output: "pantspants" |
-| **Program Removes Punctuation from User Input**| Input: "p#an^t@s  /p(ant%s" | Output: "pantspants" |
-| **Program Assigns Characters Numeric Values** | Input: "pants" | Output: "3 1 1 1 1" |
-| **Program Sums Character Values**| Input: "3 1 1 1 1" | Page Displays: 7 |
+* 1 | Program replaces 0 with "Beep!" | Example Input: 0 | Output: ["Beep!"]
+* 2 | Program replaces 1 with "Boop!" | Example Input: 1 | Output: ["Boop!"]
+* 3 | Program replaces numbers divisible by 3 with "I'm sorry, Dave. I'm afraid I can't do that." | Example Input: 3 | Output: ["I'm sorry, Dave. I'm afraid I can't do that."]
 
 ## Setup
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,49 @@
+//Problem: Create web app that takes a number from user and returns a rnge of numbers from 0 to the choosen number.
+
+// Exceptions:
+// Numbers that contain 0, replace all digits with "Beep!"
+// Numbers that contain a 1 replace all digits with "Boop!"
+//Numbers divisible by 3 replace with "I'm sorry, Dave. I'm afraid I can't do that."
+
+// Exceptions are listed from least to most important
+// first exception applies unless second does, and the same with the second & third
+
+//user should be able to enter a new number and see new results over & over again
+
+var numberZero = "0";
+var wordBeep = numberZero.replace("0", "Beep!");
+console.log(wordBeep);
+
+var numberOne = "1";
+var wordBoop = numberOne.replace("1", "Boop!");
+console.log(wordBoop);
+
+var numberThree = "3";
+var wordImSorry = numberThree.replace("3", "I'm sorry, Dave. I'm afraid I can't do that.");
+console.log(wordImSorry);
+
+// User Interface Logic
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
 
-    $(#output).show();
+    var userInput = $("#userInput").val();
+
+    $("#list").text();
+    if (userInput === 0) {
+      return "Beep!"
+    } else {
+      return userInput
+    }
+
+/*
+    $("#list").show();
+    if ("#userInput" === 0) {
+    $("#list").append("<li>Beep!</li>");
+    } else {
+    $("#list").append("Beep!");
+    }
+*/
+
   });
 });
